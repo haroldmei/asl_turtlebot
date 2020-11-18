093997717f2f49374201e6d0d01a402157a75b01
harold mei <haroldmei.cn@gmail.com>
2020-11-18 21:06:59 +1100

    navigator is on

--- scripts/navigator.py
+++ scripts/navigator.py
@@ -65,8 +65,8 @@ class Navigator:
         self.plan_start = [0.,0.]
         
         # Robot limits
-        self.v_max = rospy.get_param("~v_max")    # 0.2    # maximum velocity
-        self.om_max = rospy.get_param("~om_max")   # 0.4   # maximum angular velocity
+        self.v_max = rospy.get_param("~v_max", 0.2)    # 0.2    # maximum velocity
+        self.om_max = rospy.get_param("~om_max", 0.4)   # 0.4   # maximum angular velocity
 
         self.v_des = 0.12   # desired cruising velocity
         self.theta_start_thresh = 0.05   # threshold in theta to start moving forward when path-following
