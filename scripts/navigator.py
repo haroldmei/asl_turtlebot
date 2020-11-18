b641ea2813c2b81ff7fef2eb5d469ef5fcc044c1
harold mei <haroldmei.cn@gmail.com>
2020-11-18 21:24:51 +1100

    wat

--- scripts/navigator.py
+++ scripts/navigator.py
@@ -16,7 +16,8 @@ from controllers import PoseController, TrajectoryTracker, HeadingController
 from enum import Enum
 
 from dynamic_reconfigure.server import Server
-from asl_turtlebot.cfg import NavigatorConfig, DetectedObjectList
+from asl_turtlebot.cfg import NavigatorConfig
+from asl_turtlebot.msg import DetectedObject, DetectedObjectList
 
 # state machine modes, not all implemented
 class Mode(Enum):
